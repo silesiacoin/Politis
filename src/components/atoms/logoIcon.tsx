f7489608f591b6c784d382928386ebc07dc57589
rekeye <12szymon.paluch34@gmail.com>
2021-11-08 19:59:53 +0100

    Fix image importing error

--- src/components/atoms/logoIcon.tsx
+++ src/components/atoms/logoIcon.tsx
@@ -1,5 +1,6 @@
 import React, { ReactElement } from 'react';
-import sscLogo from '../../assets/images/ssc.png';
+
+const sscLogo = require('../../assets/images/ssc.png');
 
 const LogoIcon = (): ReactElement => <img src={sscLogo} alt="icon of silesia coin" />;
 
