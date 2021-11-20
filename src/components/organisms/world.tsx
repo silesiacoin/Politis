51b869a6e1942f24a35a74e898bbcfa58061bd65
Thomson <thomson@silesiacoin.org>
2021-11-20 03:23:25 +0100

    Correct code

--- src/components/organisms/world.tsx
+++ src/components/organisms/world.tsx
@@ -3,6 +3,7 @@ import Map from 'ol/Map';
 import 'ol/ol.css';
 import * as turf from '@turf/turf';
 import Fill from 'ol/style/Fill';
+import Text from 'ol/style/Text';
 import Stroke from 'ol/style/Stroke';
 import Style from 'ol/style/Style';
 import * as proj from 'ol/proj';
@@ -12,21 +13,8 @@ import { createMap } from '../../functions/createMap';
 const berlinMapCor = [13.44, 52.51];
 const mapZoom = 11;
 
-const highlightStyle = new Style({
-  fill: new Fill({
-    color: 'rgba(255,255,255,0.7)'
-  }),
-  stroke: new Stroke({
-    color: '#3399CC',
-    width: 3
-  })
-});
-
 export default function World(): ReactElement {
   const [startCreateMap, setStart] = useState<boolean>(true);
-  const [selected, setSelected] = useState<null | Tile>(null);
-  const [xy, setXY] = useState<{ x: number, y: number } | null>(null);
-  const [turnOnMouseMovePanel, setTurnOnMouseMovePanel] = useState<boolean>(false);
   const [onMap, setOnMap] = useState(true);
 
   function zoomMap(map: Map) {
@@ -45,16 +33,14 @@ export default function World(): ReactElement {
 
   useEffect(() => {
     if (startCreateMap) {
-      const allTiles = createTiles();
+      const allTiles: turf.helpers.Feature<turf.helpers.Polygon, Tile>[] = createTiles();
       const features = turf.featureCollection(allTiles);
       const map = createMap(features);
       zoomMap(map);
       let selectedTiles: { setStyle: (arg0: undefined) => void; } | null = null;
 
       map.on('singleclick', function (e) {
-        map.forEachFeatureAtPixel(e.pixel, function (f) {
-          console.log('click click: ' + f.get('id'),)
-
+        map.forEachFeatureAtPixel(e.pixel, function () {
           return true;
         });
         return true;
@@ -64,32 +50,29 @@ export default function World(): ReactElement {
         if (selectedTiles !== null) {
           selectedTiles.setStyle(undefined);
           selectedTiles = null;
-          setTurnOnMouseMovePanel(false);
         }
 
         map.forEachFeatureAtPixel(e.pixel, function (f: any) {
           selectedTiles = f;
-          const tile: Tile = {
-            id: f.get('id'),
-            owner: f.get('owner'),
-            price: f.get('price'),
-            polygon: f.get('polygon'),
-          };
-          setSelected(tile);
+          const myText = 'id: ' + f.get('id');
+          const highlightStyle = new Style({
+            fill: new Fill({
+              color: 'rgba(255,255,255,0.7)'
+            }),
+            stroke: new Stroke({
+              color: '#3399CC',
+              width: 3
+            }),
+            text: new Text({
+              text: myText
+            })
+          });
           f.setStyle(highlightStyle);
-          setTurnOnMouseMovePanel(true);
-
-          document.onmousemove = function (e) {
-            const x = e.clientX;
-            const y = e.clientY;
-            setXY({ x: x + 10, y: y + 10 });
-          }
 
           return true;
         });
         return true;
       });
-
 
       return setStart(false);
     }
@@ -101,21 +84,10 @@ export default function World(): ReactElement {
         setStart(!startCreateMap);
         setOnMap(!onMap);
       }}>
-        refresh
+        refresh map
       </button>
       {onMap &&
         <div id={'map'} className={'map'}>
-          {turnOnMouseMovePanel &&
-            <div className={'select-tile-panel'} style={{
-              top: xy?.y,
-              left: xy?.x
-            }}>
-              <div>id: {selected?.id}</div>
-              <div>owner: {selected?.owner}</div>
-              <div>price: {selected?.price}</div>
-              <div>polygon: {selected?.polygon[0][0].toFixed(2)} {selected?.polygon[0][1].toFixed(2)} {selected?.polygon[1][1].toFixed(2)} {selected?.polygon[1][0].toFixed(2)}</div>
-            </div>
-          }
         </div>
       }
     </>
